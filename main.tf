b57009c3b6a5650381cc310ac9a3281a5536db53
stefoto85 <you@example.com>
2026-03-28 20:39:27 +0200

    add tfstate

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.66.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "Storagerg"
+    storage_account_name = "taskboardstoragedimov"
+    container_name = "taslboarcontainer"
+    key = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
